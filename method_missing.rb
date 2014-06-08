d45987f808a4b5710292837a9c196e908d354254
gomenasaai <contramundee@gmail.com>
2014-06-08 21:38:07 +0100

    exploring method_missing

--- method_missing.rb
+++ method_missing.rb
@@ -4,9 +4,15 @@ class B
 	end
 
 	def method_missing method, *args, &block
-		super method, *args, &block
+		if method.to_s.include? "name"
+			name
+		else
+			super method, *args, &block
+		end
 	end
 
 end
 
+puts B.new.name_method
+puts B.new.whats_my_name
 puts B.new.age
